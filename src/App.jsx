904f411d0c1cabaf8beb2b0c9d8c80f90dab6fdc
thathariss <hkhan.khan14007@gmail.com>
2024-11-12 02:35:47 +0530

    added form submission and validation by useState, useRef, in-built form, and customHooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Header from './components/Header.jsx';
-import Login from './components/Login.jsx';
+import Header from "./components/Header.jsx";
+import Login from "./components/StateLogin.jsx";
 
 function App() {
   return (
@@ -7,6 +7,7 @@ function App() {
       <Header />
       <main>
         <Login />
+        {/* <Signup /> */}
       </main>
     </>
   );
